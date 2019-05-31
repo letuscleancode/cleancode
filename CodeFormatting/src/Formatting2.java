bbf5e9fe28e6cca43ae041bf5a91ac2c17d49f92
letuscleancode <50235922+letuscleancode@users.noreply.github.com>
2019-05-31 17:00:20 +0530

    Formtting Lab

---
+++ CodeFormatting/src/Formatting2.java
@@ -0,0 +1,54 @@
+public class Formatting {
+private class UnFormattedCode {
+
+ public List<String> readFile() throws IOException {try(BufferedReader br = new BufferedReader(new FileReader("original.txt"))){
+            List<String> listOfData = new ArrayList()<>();String d;
+            while((d = br.readLine()) != null){
+            listOfData.add(d);            }
+            return listOfData;
+        }
+    }
+
+    public static void writeFile(List<String> listOfData) throws IOException{
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("numbers.txt"))){
+            for(String str: listOfData){
+                bw.write(str);bw.newLine();            }
+        }
+    }
+    public  void main() throws IOException {
+        List<String> data = readFile(); writeFile(data);
+    }
+}
+
+
+Clean COde
+
+
+private class FormattedCode {
+
+ public  List<String> readFile() throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader("original.txt"))){
+            List<String> listOfData = new ArrayList()<>();
+            String d;
+            while((d = br.readLine()) != null){
+                listOfData.add(d);
+            }
+            return listOfData;
+        }
+    }
+
+    public  void writeFile(List<String> listOfData) throws IOException{
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("numbers.txt"))){
+            for(String str: listOfData){
+                bw.write(str);
+                bw.newLine();
+            }
+        }
+    }
+
+    public  void main() throws IOException {
+        List<String> data = readFile();
+        writeFile(data);
+    }
+}
+}
